f402e7a1d359a4075cf6abf5c6df73ae9a1b241d
Zigalow <s224302@dtu.dk>
2024-03-22 11:00:06 +0100

    Added ChatHub.cs placeholder code

---
+++ Turnbased-Game/Hubs/ChatHub.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.SignalR;
+
+namespace Turnbased_Game.Hubs;
+
+public class ChatHub : Hub
+{
+    public Task SendMessage(string user, string message)
+    {
+        return Clients.All.SendAsync("ReceiveMessage", user, message);
+    }
+}
