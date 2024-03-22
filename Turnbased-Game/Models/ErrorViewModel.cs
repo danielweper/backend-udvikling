ffd97274a257458532e783deba6e2f960414f028
Zigalow <s224302@dtu.dk>
2024-03-22 11:59:54 +0100

    Using MVC instead of razor pages

---
+++ Turnbased-Game/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Turnbased_Game.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
