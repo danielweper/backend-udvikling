3ba4acb74faca9d119a22ce73b157f3ff635c8e9
Zigalow <s224302@dtu.dk>
2024-04-12 12:17:40 +0200

    Removed errors

--- Turnbased-Game/Models/Client/IFighter.cs
+++ Turnbased-Game/Models/Client/IFighter.cs
@@ -2,9 +2,10 @@ using Turnbased_Game.Models.Server;
 
 namespace Turnbased_Game.Models.Client;
 
-public interface IChatter
+public interface IFighter
+
 {
-    public event Func BattleIsOver;
+    public event Func<string> BattleIsOver; // check
     public event Func<string> TurnIsOver;
 
     public void SubmitTurn(string turn);
--- Turnbased-Game/Models/Client/IParticipant.cs
+++ Turnbased-Game/Models/Client/IParticipant.cs
@@ -3,7 +3,7 @@ namespace Turnbased_Game.Models.Client;
 public interface IParticipant
 {
     public event Func<string> JoinedLobby;
-    public event Func LeftLobby;
+    public event Func<byte> LeftLobby; // check
     public event Func<byte, IPlayerProfile> PlayerJoined;
     public event Func<byte> PlayerLeft;
     public event Func<ulong> GameStarting;  // maybe DateTime instead of ulong
