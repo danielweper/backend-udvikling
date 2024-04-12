fa82808baf3497a71b68f7cdd95651590aa1e526
Zigalow <s224302@dtu.dk>
2024-04-12 12:21:17 +0200

    Changed from void to Task Made server implement IServerPackage

--- Turnbased-Game/Models/Server/IServer.cs
+++ Turnbased-Game/Models/Server/IServer.cs
@@ -1,13 +1,16 @@
+using Turnbased_Game.Models.Packages;
+using Turnbased_Game.Models.Packages.Server;
+
 namespace Turnbased_Game.Models.Server;
 
-public interface IServer
+public interface IServer : IServerPackage
 {
-    public void Ping();
-    public void Acknowledge();
-    public void Accepted();
-    public void Denied();
-    public void InvalidRequest(string natureOfError);
+    Task ReceiveMessage(string user, string message);
+    Task Acknowledge(IAcknowledged message);
+    Task Accepted();
+    Task Denied();
+    Task InvalidRequest(string natureOfError);
 
-    public void UserMessage(IUserMessage content);
-    public void SystemMessage(ISystemMessage content);
+    Task UserMessage(IUserMessage content);
+    Task SystemMessage(ISystemMessage content);
 }
