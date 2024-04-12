7bd363cdf202b55d1b3c244f2411aac37895eb64
Zigalow <s224302@dtu.dk>
2024-04-12 14:10:31 +0200

    Added parameters

--- Turnbased-Game/Models/Server/IServer.cs
+++ Turnbased-Game/Models/Server/IServer.cs
@@ -7,9 +7,9 @@ public interface IServer : IServerPackage
 {
     Task ReceiveMessage(string user, string message);
     Task Acknowledge(IAcknowledged message);
-    Task Accepted();
-    Task Denied();
-    Task InvalidRequest(string natureOfError);
+    Task Accepted(IAccepted content);
+    Task Denied(IDenied content);
+    Task InvalidRequest(IInvalidRequest content);
 
     Task UserMessage(IUserMessage content);
     Task SystemMessage(ISystemMessage content);
