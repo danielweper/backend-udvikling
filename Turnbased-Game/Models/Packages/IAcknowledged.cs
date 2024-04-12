dae6df2f15f116c8e6d6f38d0578de04bbf94469
Zigalow <s224302@dtu.dk>
2024-04-12 12:24:28 +0200

    Added properties for IAcknowledged.cs

--- Turnbased-Game/Models/Packages/IAcknowledged.cs
+++ Turnbased-Game/Models/Packages/IAcknowledged.cs
@@ -1,6 +1,7 @@
 namespace Turnbased_Game.Models.Packages;
 
-public interface IAcknowledged: IPackage
+public interface IAcknowledged : IPackage
 {
-    
+    public string AckMessage { get; set; }
+    public DateTime Timestamp { get; set; }
 }
