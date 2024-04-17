fc2c2073e70ad6b32b162df60187a24d40bbf18d
Zaid Sheikh <Zaidsh02@gmail.com>
2024-04-17 22:17:55 +0200

    added more properties

--- Turnbased-Game/Models/Packages/IDenied.cs
+++ Turnbased-Game/Models/Packages/IDenied.cs
@@ -2,5 +2,5 @@ namespace Turnbased_Game.Models.Packages;
 
 public interface IDenied: IPackage
 {
-    
+    public int RequestId { get; }
 }
--- Turnbased-Game/Models/Packages/IInvalidRequest.cs
+++ Turnbased-Game/Models/Packages/IInvalidRequest.cs
@@ -2,5 +2,6 @@ namespace Turnbased_Game.Models.Packages;
 
 public interface IInvalidRequest: IPackage
 {
-    
+    public int RequestId { get; }
+    public string ErrorMessage { get; }
 }
