d02e64fba7a1c0ff0beebc5a351af116a747d6e0
Zigalow <s224302@dtu.dk>
2024-04-18 23:25:01 +0200

    Namespaces and small changes

--- Turnbased-Game/Models/Client/IHost.cs
+++ Turnbased-Game/Models/Client/IHost.cs
@@ -1,15 +1,17 @@
+using Turnbased_Game.Models.Packages.Client;
+
 namespace Turnbased_Game.Models.Client;
 
 public interface IHost : IParticipant
 {
     public event Func<byte, IRole> RoleChangeRequested;
 
-    public Task<byte> CreateLobby();
+    public Task<byte> ReceiveLobby(CreateLobbyRequest request);
     public void ChangeGameSettings(string settings); // JSON
     public void KickPlayer(int playerId, string reason);
     public void CreateGame();
     public void DeleteGame();
     public void StartGame();
     public void Accepted(int requestId);
-    public void Denied(int requestId);
+    public Task Denied(int requestId);
 }
