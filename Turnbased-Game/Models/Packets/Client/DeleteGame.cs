037d4a4980268bbebb94636777b6ee7b82e7fb36
Felix Schmidt <s224313@dtu.dk>
2024-04-19 10:01:30 +0200

    packet id's same as protocol

--- Turnbased-Game/Models/Packets/Client/DeleteGame.cs
+++ Turnbased-Game/Models/Packets/Client/DeleteGame.cs
@@ -2,7 +2,7 @@ namespace Turnbased_Game.Models.Packets.Client;
 
 public class DeleteGame : IPackage
 {
-    public byte id { get; } = 42;
+    public byte id { get; } = 36;
     public string gameName { get; set; }
     
 }
--- Turnbased-Game/Models/Packets/Client/StartGame.cs
+++ Turnbased-Game/Models/Packets/Client/StartGame.cs
@@ -2,5 +2,5 @@ namespace Turnbased_Game.Models.Packets.Client;
 
 public class StartGame: IPackage
 {
-   public byte id { get; } = 99;
+   public byte id { get; } = 22;
 }
