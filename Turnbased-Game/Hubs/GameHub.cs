cb044b3df419970b134bf0422269b20cc8bfbe37
Zaid Sheikh <Zaidsh02@gmail.com>
2024-04-21 17:38:31 +0200

    Create most of the lobby methods

--- Turnbased-Game/Hubs/GameHub.cs
+++ Turnbased-Game/Hubs/GameHub.cs
@@ -45,7 +45,6 @@ public class GameHub : Hub<IClient>
             return;
         }
 
-
         Player player = new Player(GenerateParticipantId(lobby));
 
         lobby.AddPlayer(player);
@@ -59,8 +58,100 @@ public class GameHub : Hub<IClient>
             .PlayerHasJoined(new PlayerJoinedLobbyPacket(playerId: client.id, new PlayerProfile()));
         await Groups.AddToGroupAsync(Context.ConnectionId, $"{lobbyId}");
     }
+    
+    public async Task KickPlayerFromLobby(byte playerId, string reason, byte lobbyId)
+    {
+        Lobby? lobby = _server.GetLobby(lobbyId);
+
+        if (lobby == null)
+        {
+            await SendMessagePacket(caller: Clients.Caller, message: "No lobby found with corresponding id",
+                type: MessageType.Denied);
+            return;
+        }
+        Player? player = lobby.players.FirstOrDefault(p => p.id == playerId);
+
+        if (player != null)
+        {
+            //Remove the player from the lobby
+            lobby.RemovePlayer(player);
+
+            await SendMessagePacket(
+                message: $"You have successfully kicked player {player.id} from this lobby: {lobbyId}",
+                type: MessageType.Accepted, caller: Clients.Caller);
+            await Clients.Group($"{lobbyId}").KickPlayer(playerId: player.id, reason: reason);
+        }
+        else
+        {
+            await SendMessagePacket(caller: Clients.Caller, message: $"The player is not in this lobby: {lobbyId}",
+                type: MessageType.Denied);
+        }
+    }
+    public async Task LeaveLobby(byte lobbyId)
+    {
+        Lobby? lobby = _server.GetLobby(lobbyId);
+
+        if (lobby == null)
+        {
+            await SendMessagePacket(caller: Clients.Caller, message: "This lobby does not exist",
+                type: MessageType.Denied);
+            return;
+        }
+        
+        Player? player = lobby.players.FirstOrDefault(p => p.id.ToString() == Context.ConnectionId);
+
+        if (player != null)
+        {
+            //Remove Player
+            lobby.RemovePlayer(player);
+            
+            //send packet
+            await Clients.Caller.DisconnectLobby(player.id);
+            await SendMessagePacket(message: $"You have successfully disconnected from the lobby: {lobbyId}", type: MessageType.Accepted,
+                caller: Clients.Caller);
+        }
+        else
+        {
+            await SendMessagePacket(caller: Clients.Caller, message: "You are not in this lobby",
+                type: MessageType.Denied);
+        }
+    }
+    public async Task ViewAvailableLobbies()
+    {
+        await SendMessagePacket("Received view AvailableLobbies", MessageType.Acknowledged,
+            Clients.Caller); // Acknowledged
+        
+        //Get all the Lobbies from server
+        string[] lobbiesInfo = _server.GetAvailableLobbies().ToArray();
+        AvailableLobbiesPacket availableLobbiesPacketPacket = new AvailableLobbiesPacket(lobbiesInfo);
+        
+        //Send the packet to the client
+        await Clients.Caller.ListAvailableLobbies(availableLobbiesPacketPacket);
+    }
+
+    public async Task CreateGame(byte lobbyId)
+    {
+        // Acknowledged
+        await SendMessagePacket("Received CreateGame request", MessageType.Acknowledged, Clients.Caller);
+
+        // Create lobby
 
 
+        Lobby? lobby = _server.GetLobby(lobbyId);
+
+        if (lobby != null)
+        {
+            lobby.CreateGame();
+
+            await Clients.Group("lobbyId").CreateGame();
+            await SendMessagePacket("Game created", MessageType.Accepted, Clients.Caller);
+        }
+        else
+        {
+            await SendMessagePacket("The lobby doesn't exist", MessageType.Denied, Clients.Caller);
+        }
+    }
+    
     private byte GenerateLobbyId()
     {
         byte lobbyId;
--- Turnbased-Game/Models/Server/Lobby.cs
+++ Turnbased-Game/Models/Server/Lobby.cs
@@ -12,16 +12,17 @@ public class Lobby
     public Player host => _players[0];
     public IReadOnlyList<Player> players => _players.AsReadOnly();
     private List<Player> _players;
-
-    public Lobby(byte id, Player host, int maxPlayerCount = 10)
+    private List<Game> _games;
+    public LobbyVisibility visibility { get; private set; }
+    public Lobby(byte id, Player host, int maxPlayerCount = 10, LobbyVisibility visibility = LobbyVisibility.Public)
     {
         this.id = id;
         _players = new List<Player>();
         _players.Add(host);
-
         this.maxPlayerCount = maxPlayerCount;
+        this.visibility = visibility;
     }
-
+    
     public LobbyInfo GetInfo()
     {
         return new LobbyInfo(id, host, _players, maxPlayerCount);
@@ -31,4 +32,23 @@ public class Lobby
     {
         _players.Add(player);
     }
+    public void RemovePlayer(Player player)
+    {
+        _players.Remove(player);
+    }
+    public void CreateGame()
+    {
+        Game newGame = new Game();
+        _games.Add(newGame);
+        
+    }
+    public void LeaveGame(Game game)
+    {
+        
+    }
+}
+public enum LobbyVisibility
+{
+    Public,
+    Private
 }
