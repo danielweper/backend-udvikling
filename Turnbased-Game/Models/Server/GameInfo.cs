37749e5528870260839d7c95d6021997161f9640
Zigalow <s224302@dtu.dk>
2024-04-23 21:12:03 +0200

    Added GameInfo and GameInfoPacket

---
+++ Turnbased-Game/Models/Server/GameInfo.cs
@@ -0,0 +1,13 @@
+namespace Turnbased_Game.Models.Server;
+
+public readonly struct GameInfo(GameSettings gameSettings, List<Battle> battle)
+{
+    public GameSettings GameSettings => gameSettings;
+    public List<Battle> Battles => battle;
+
+
+    public GameInfo(Game game) : this(game.settings, game.battles)
+    {
+        
+    }
+}
