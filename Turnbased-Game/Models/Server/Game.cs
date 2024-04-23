c767d59c55ac6b51c00ed4f84238614c761c1c9d
Zigalow <s224302@dtu.dk>
2024-04-23 21:16:43 +0200

    Added GetInfo to get a GameInfo

--- Turnbased-Game/Models/Server/Game.cs
+++ Turnbased-Game/Models/Server/Game.cs
@@ -3,5 +3,10 @@ namespace Turnbased_Game.Models.Server;
 public class Game(GameType gameType)
 {
     public GameSettings settings { get; set; } = new GameSettings(gameType);
-    public List<Battles> battles { get; set; } = new List<Battles>();
+    public List<Battle> battles { get; set; } = new List<Battle>();
+
+    public GameInfo GetInfo()
+    {
+        return new GameInfo(settings, battles);
+    }
 }
