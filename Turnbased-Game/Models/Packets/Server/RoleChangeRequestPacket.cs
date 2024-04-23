84722f43a598c34150ddd1ba62ad19e604d2eef5
Zigalow <s224302@dtu.dk>
2024-04-23 23:42:38 +0200

    Cleanup

--- Turnbased-Game/Models/Packets/Server/RoleChangeRequestPacket.cs
+++ Turnbased-Game/Models/Packets/Server/RoleChangeRequestPacket.cs
@@ -1,10 +1,11 @@
+using Turnbased_Game.Models.Client;
 using Turnbased_Game.Models.Packets.Shared;
 using Turnbased_Game.Models.Server;
 
 namespace Turnbased_Game.Models.Packets.Server;
 
-public class RoleChangeRequestPacket(byte playerId, PlayerRole requestedRole, IRole requestedRole1) : IPackage
+public class RoleChangeRequestPacket(byte playerId, PlayerRole requestedRole) : IPackage
 {
     public byte PlayerId { get; } = playerId;
-    public IRole RequestedRole { get; } = requestedRole1;
+    public PlayerRole RequestedRole { get; } = requestedRole;
 }
