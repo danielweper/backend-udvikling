d1e1090c93f3be1cc6fdef612a21105a1c51a01c
Zigalow <s224302@dtu.dk>
2024-04-23 23:48:30 +0200

    Added ConnectionId to player, so that each client can be called from their respective player

--- Turnbased-Game/Models/Server/Player.cs
+++ Turnbased-Game/Models/Server/Player.cs
@@ -2,15 +2,16 @@ using Turnbased_Game.Models.Client;
 
 namespace Turnbased_Game.Models.Server;
 
-public class Player(byte id)
+public class Player(string connectionId, byte participantId)
 {
-    public byte id { get; set; } = id;
+    public string ConnectionId { get; init; } = connectionId;
+    public byte ParticipantId { get; set; } = participantId;
     public PlayerRole Role { get; set; }
-    public PlayerProfile Profile { get; set; } 
+    public PlayerProfile Profile { get; set; }
     public bool ReadyStatus { get; set; }
-    
+
     public enum PlayerRole
-    { 
+    {
         Spectator,
         Fighter
     }
