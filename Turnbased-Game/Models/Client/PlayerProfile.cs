9a8dbf5e6deff197c7169bd100246d38960dffcc
Zigalow <s224302@dtu.dk>
2024-04-23 23:24:27 +0200

    Added toString to PlayerProfile.cs

--- Turnbased-Game/Models/Client/PlayerProfile.cs
+++ Turnbased-Game/Models/Client/PlayerProfile.cs
@@ -1,6 +1,16 @@
+using System.Text;
+
 namespace Turnbased_Game.Models.Client;
 
 public class PlayerProfile : IPlayerProfile
 {
-    
+    public override string ToString()
+    {
+        StringBuilder sb = new StringBuilder();
+
+        sb.AppendLine($"Color: {Color}");
+        sb.AppendLine($"Name: {Name}");
+        
+        return sb.ToString();
+    }
 }
