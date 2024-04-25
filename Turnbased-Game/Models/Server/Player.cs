71286001e4323b186a4b618709b733ae1b089f74
Zigalow <s224302@dtu.dk>
2024-04-25 23:34:25 +0200

    Changed constructor setup

--- Turnbased-Game/Models/Server/Player.cs
+++ Turnbased-Game/Models/Server/Player.cs
@@ -2,18 +2,13 @@ using Turnbased_Game.Models.Client;
 
 namespace Turnbased_Game.Models.Server;
 
-public class Player(string displayName, byte participantId)
+public class Player(string displayName, byte participantId, PlayerProfile playerProfile)
 {
     public string DisplayName { get; set; } = displayName;
     public byte ParticipantId { get; set; } = participantId;
     public PlayerRole Role { get; set; }
-    public PlayerProfile Profile { get; set; }
+    public PlayerProfile Profile { get; set; } = playerProfile;
     public bool ReadyStatus { get; set; }
-
-    public Player(string displayName, byte participantId, PlayerProfile profile) : this(displayName, participantId)
-    {
-        Profile = profile;
-    }
 
     public enum PlayerRole
     {
