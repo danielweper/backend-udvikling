21fc0f5ec525a3a599835c73881bfac579041c1a
Zigalow <s224302@dtu.dk>
2024-04-25 22:36:06 +0200

    Added DisplayName

--- Turnbased-Game/Models/Packets/Server/PlayerJoinedLobbyPacket.cs
+++ Turnbased-Game/Models/Packets/Server/PlayerJoinedLobbyPacket.cs
@@ -2,9 +2,10 @@ using Turnbased_Game.Models.Packets.Shared;
 
 namespace Turnbased_Game.Models.Packets.Server;
 
-public class PlayerJoinedLobbyPacket(int playerId, PlayerProfile profile) : IPackage
+public class PlayerJoinedLobbyPacket(int playerId, string displayName, PlayerProfile profile) : IPackage
 {
     public byte PacketId => 15;
-    public int PlayerId { get; set; } = playerId;
-    public PlayerProfile Profile{ get; set; } = profile;
+    public int PlayerId { get; } = playerId;
+    public string DisplayName { get; } = displayName;
+    public PlayerProfile Profile { get; } = profile;
 }
