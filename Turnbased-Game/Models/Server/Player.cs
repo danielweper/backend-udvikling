eb37fd00862ac7e643977fd1e8138867eb7fef1b
Zigalow <s224302@dtu.dk>
2024-04-25 21:57:48 +0200

    Added DisplayName and changed constructors to accomodate DisplayName

--- Turnbased-Game/Models/Server/Player.cs
+++ Turnbased-Game/Models/Server/Player.cs
@@ -2,12 +2,18 @@ using Turnbased_Game.Models.Client;
 
 namespace Turnbased_Game.Models.Server;
 
-public class Player(byte participantId)
+public class Player(string displayName, byte participantId)
 {
+    public string DisplayName { get; set; } = displayName;
     public byte ParticipantId { get; set; } = participantId;
     public PlayerRole Role { get; set; }
     public PlayerProfile Profile { get; set; }
     public bool ReadyStatus { get; set; }
+
+    public Player(string displayName, byte participantId, PlayerProfile profile) : this(displayName, participantId)
+    {
+        Profile = profile;
+    }
 
     public enum PlayerRole
     {
