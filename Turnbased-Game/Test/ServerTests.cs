2dbbe72eda1dde9b797c6fa04ecd43cb558df86c
Zaid Sheikh <Zaidsh02@gmail.com>
2024-04-26 00:15:17 +0200

    Updated & Corrected the test

--- Turnbased-Game/Test/ServerTests.cs
+++ Turnbased-Game/Test/ServerTests.cs
@@ -1,5 +1,8 @@
 
+using Turnbased_Game.Models;
 using Turnbased_Game.Models.Server;
+using static Turnbased_Game.Models.Color;
+using Color = Turnbased_Game.Models.Color;
 using Xunit;
 
 namespace Turnbased_Game.Test;
@@ -8,17 +11,24 @@ public class ServerTests
 {
     
     [Fact]
-    public async Task Test_RemoveLobby()
+    public void Test_RemoveAndAddLobby()
     {
         // Arrange
         var server = new Server();
-        var host = new Player("Cossai",0);
-        var lobby = new Lobby( 1, host, 9, LobbyVisibility.Private);
+        
+        var playerProfile = new PlayerProfile(Red, "Cossai", "1");
+        var host = new Player("Cossai",0,playerProfile);
+        
+        var lobby = new Lobby( 1, host, 9, LobbyVisibility.Public);
+        
         //Test_AddLobby
         server.AddLobby(lobby);
         
+        //Check if lobby is added 
         Assert.Contains(server.GetAvailableLobbies(), lobbyInfo => lobbyInfo.id == lobby.Id);
         Assert.False(server.LobbyIdIsFree(lobby.Id));
+        //Check if host is added as lobby host
+        Assert.True(lobby.Host == host);
         
         // Act
         server.RemoveLobby(lobby);
