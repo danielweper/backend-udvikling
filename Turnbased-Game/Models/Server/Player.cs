575a945edb38b35adfd225cae2478e6d63a92850
Zigalow <s224302@dtu.dk>
2024-04-25 20:35:49 +0200

    Moved connectionId into playerProfile, so that a PlayerProfile can exist outside of a lobby

--- Turnbased-Game/Models/Server/Player.cs
+++ Turnbased-Game/Models/Server/Player.cs
@@ -4,7 +4,6 @@ namespace Turnbased_Game.Models.Server;
 
 public class Player(string connectionId, byte participantId)
 {
-    public string ConnectionId { get; init; } = connectionId;
     public byte ParticipantId { get; set; } = participantId;
     public PlayerRole Role { get; set; }
     public PlayerProfile Profile { get; set; }
