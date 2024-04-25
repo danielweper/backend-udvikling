39bff004f4d74bfbe4584be4433b3f75584a76f6
Zaid Sheikh <Zaidsh02@gmail.com>
2024-04-26 01:54:30 +0200

    Added test for GetAvailableLobbies

--- Turnbased-Game/Test/ServerTests.cs
+++ Turnbased-Game/Test/ServerTests.cs
@@ -1,5 +1,9 @@
 
+using Moq;
+using Turnbased_Game.Hubs;
 using Turnbased_Game.Models;
+using Turnbased_Game.Models.Packets.Server;
+using Turnbased_Game.Models.Packets.Shared;
 using Turnbased_Game.Models.Server;
 using static Turnbased_Game.Models.Color;
 using Color = Turnbased_Game.Models.Color;
@@ -9,6 +13,14 @@ namespace Turnbased_Game.Test;
 
 public class ServerTests
 {
+    private readonly GameHub _hub;
+    private Mock<IClient> _mockClient;
+
+    public ServerTests()
+    {
+        _mockClient = new Mock<IClient>();
+        _hub = new GameHub();
+    }
     
     [Fact]
     public void Test_RemoveAndAddLobby()
@@ -36,5 +48,24 @@ public class ServerTests
         // Assert
         Assert.DoesNotContain(server.GetAvailableLobbies(), lobbyInfo => lobbyInfo.id == lobby.Id);
         Assert.True(server.LobbyIdIsFree(lobby.Id));
+    }
+
+    [Fact]
+    public void Test_GetAvailableLobbies()
+    {
+        // Arrange
+        var server = new Server();
+        
+        var playerProfile = new PlayerProfile(Red, "Cossai", "1");
+        var host = new Player("Cossai",0,playerProfile);
+        
+        var lobby = new Lobby( 1, host, 9, LobbyVisibility.Private);
+        
+        // Act
+        server.AddLobby(lobby);
+        
+        //Assert
+        Assert.DoesNotContain(server.GetAvailableLobbies(), lobbyInfo => lobbyInfo.id == lobby.Id);
+        Assert.False(server.LobbyIdIsFree(lobby.Id));
     }
 }
