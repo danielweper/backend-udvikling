400a2be1c49884f19214c41390e23ea6f5f02634
Zigalow <s224302@dtu.dk>
2024-04-27 01:23:18 +0200

    Changed battle so that it's made with 1v1 in mind

--- Turnbased-Game/Models/Packets/Server/BattleIsOverPacket.cs
+++ Turnbased-Game/Models/Packets/Server/BattleIsOverPacket.cs
@@ -2,9 +2,9 @@ using Turnbased_Game.Models.Server;
 
 namespace Turnbased_Game.Models.Packets.Server;
 
-public class BattleIsOverPacket(bool battleFinished, List<Player> battleWinner): IServerPackage
+public class BattleIsOverPacket(bool battleFinished, Player? battleWinner): IServerPackage
 {
     public byte PacketId => 33;
     public bool BattleFinished { get; set; } = battleFinished;
-    public List<Player> BattleWinner { get; } = battleWinner;
+    public Player? BattleWinner { get; } = battleWinner;
 }
--- Turnbased-Game/Models/Server/Battle.cs
+++ Turnbased-Game/Models/Server/Battle.cs
@@ -3,36 +3,54 @@ namespace Turnbased_Game.Models.Server;
 public class Battle
 {
     public byte BattleId { get; }
-    public List<Player> Figthers { get; set; }
-    public List<Player> Winners { get; set; }
+
+    // public List<Player> Fitghers { get; set; }
+    public Player[] Fighters { get; } = new Player[2];
+
+    // public List<Player> Winners { get; set; }
+    public Player? Winner { get; private set; }
+
     public bool IsDone { get; set; }
+
+    // public readonly List<Player> PlayersToExecuteTurn;
     public readonly List<Player> PlayersToExecuteTurn;
-    
+
+
     public Battle(byte battleId)
     {
         BattleId = battleId;
-        Figthers = new List<Player>(2);
-        Winners = new List<Player>();
+        // Fighters = new List<Player>(2);
+        // Winners = new List<Player>();
         IsDone = false;
-        PlayersToExecuteTurn = Figthers;
+        PlayersToExecuteTurn = Fighters.ToList();
     }
-    
-    public void AddPlayer(Player player)
+
+    public Battle(byte battleId, Player player1, Player player2)
+    {
+        BattleId = battleId;
+        Fighters[0] = player1;
+        Fighters[1] = player2;
+        IsDone = false;
+        PlayersToExecuteTurn = Fighters.ToList();
+    }
+
+    /*public void AddPlayer(Player player)
     {
         if (Figthers.Count >= 2) return;
         Figthers.Add(player);
         PlayersToExecuteTurn.Add(player);
         player.Health = 100;
-    }
-    
+    }*/
+
+
     public void ExecuteRound()
     {
-        foreach (var player in Figthers)
+        foreach (var player in Fighters)
         {
             ExecutePlayerTurn(player);
         }
-        
     }
+
     private void ExecutePlayerTurn(Player player)
     {
         switch (player.FightAction)
@@ -49,11 +67,13 @@ public class Battle
 
         player.ExecutedStatus = true;
     }
+
     private void Attack(Player attacker)
     {
-        var target = Figthers.Find(p => p.ParticipantId != attacker.ParticipantId);
+        // var target = Fighters.Find(p => p.ParticipantId != attacker.ParticipantId);
+        var target = Fighters[0] != attacker ? Fighters[0] : Fighters[1];
 
-        if (target == null) return;
+        // if (target == null) return;
         attacker.Attack = 10;
         var damage = attacker.Attack - target.Defense;
         if (damage > 0)
@@ -61,6 +81,7 @@ public class Battle
             target.Health -= damage;
         }
     }
+
     private static void Defend(Player defender)
     {
         defender.Defense = 5;
@@ -68,10 +89,24 @@ public class Battle
 
     public bool HasAWinner()
     {
-        Winners = Figthers.Where(p => p.Health > 0).ToList();
+        /*Winners = Figthers.Where(p => p.Health > 0).ToList();
         IsDone = Winners.Count == 1;
-        return IsDone;
+        return IsDone;*/
+
+        if (Fighters[0].Health <= 0)
+        {
+            Winner = Fighters[0];
+        }
+        else if (Fighters[1].Health <= 0)
+        {
+            Winner = Fighters[1];
+        }
+
+        IsDone = true;
+
+        return true;
     }
+
     public void UpdateExecutedTurn(Player player)
     {
         PlayersToExecuteTurn.Remove(player);
@@ -80,5 +115,11 @@ public class Battle
     public List<Player> GetPlayersToExecuteTurn()
     {
         return PlayersToExecuteTurn;
+    }
+
+    public void EndGamePrematurely(Player loser)
+    {
+        IsDone = true;
+        Winner = loser != Fighters[0] ? Fighters[0] : Fighters[1];
     }
 }
