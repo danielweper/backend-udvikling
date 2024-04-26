e3567ea4357d282d76eb510450a4b8c19116fa6a
Zaid Sheikh <Zaidsh02@gmail.com>
2024-04-26 09:11:40 +0200

    GetBattle method added in Game

--- Turnbased-Game/Models/Server/Game.cs
+++ Turnbased-Game/Models/Server/Game.cs
@@ -2,11 +2,23 @@ namespace Turnbased_Game.Models.Server;
 
 public class Game(GameType gameType)
 {
-    public GameSettings settings { get; set; } = new GameSettings(gameType);
-    public List<Battle> battles { get; set; } = new List<Battle>();
-
+    public GameSettings Settings { get; set; } = new GameSettings(gameType);
+    public List<Battle> Battles { get; set; } = new List<Battle>();
+    
     public GameInfo GetInfo()
     {
-        return new GameInfo(settings, battles);
+        return new GameInfo(Settings, Battles);
     }
+    public Battle? GetBattle(byte requestId)
+    {
+        foreach (var battle in Battles)
+        {
+            if (battle.BattleId == requestId)
+            {
+                return battle;
+            }
+        }
+        return null;
+    }
+    
 }
